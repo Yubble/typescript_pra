1e12986d0284bb7c686c7cc04cd4b2cb9d101d51
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-01-05 15:48:02 +0800

    feat: 更细类型保护

--- src/fromBook/typeProtect.js
+++ src/fromBook/typeProtect.js
@@ -2,9 +2,29 @@
  * 类型保护
 */
 function getPerson() {
-    return {};
+    return {
+        speech: function () {
+            console.log('我是教学的');
+        },
+        learn: function () {
+            console.log('我可以学习');
+        },
+        teach: function () {
+            console.log('我是教学的');
+        }
+    };
 }
+// 如果是函数使用接口，则必须要有两个接口的并集，且出现一个接口特有方法
 var stu = getPerson();
 stu.speech();
 stu.learn();
 stu.teach();
+function isTeacher(person) {
+    return person.teach !== undefined;
+}
+if (isTeacher(stu)) {
+    stu.teach();
+}
+else {
+    stu.learn();
+}
--- src/fromBook/typeProtect.ts
+++ src/fromBook/typeProtect.ts
@@ -13,11 +13,34 @@ interface Student {
 }
 
 function getPerson(): Teacher | Student {
-  return {} as Teacher
+  return {
+    speech() {
+      console.log('我是教学的')
+    },
+    learn() {
+      console.log('我可以学习')
+    },
+    teach() {
+      console.log('我是教学的')
+    }
+  }
 }
+
+// 如果是函数使用接口，则必须要有两个接口的并集，且出现一个接口特有方法
 
 let stu = getPerson()
 
 stu.speech();
 (<Student>stu).learn();
 (<Teacher>stu).teach();
+
+// 必须要加person is Teacher主谓宾，不然之后无法使用stu直接调用teach和learn方法
+function isTeacher(person: Teacher | Student): person is Teacher {
+  return (<Teacher>person).teach !== undefined
+}
+
+if (isTeacher(stu)) {
+  stu.teach()
+} else {
+  stu.learn()
+}
